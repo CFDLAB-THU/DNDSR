01e4b3ee01cb433969db0a533e23daaf8870f99c
HarryZhou <2373256746@qq.com>
2025-04-09 00:18:27 +0800

    dev: BG grid point  dist using interpolation now working Bad thing: workload is super unbalanced due to BG being uniform!!!

--- test/Geom/OversetCart/OversetCart.py
+++ test/Geom/OversetCart/OversetCart.py
@@ -7,15 +7,31 @@ from utils import *
 
 
 class OversetPart2D:
-    def __init__(self, mpi: DNDS.MPIInfo):
+    def __init__(self, mpi: DNDS.MPIInfo, transform=(np.eye(3, 3), np.zeros(3))):
         self._mpi = mpi
         self._MPI = get_mpi4py_comm_from_MPIInfo(mpi)
+        self._transform = transform
+        assert transform[0].shape == (3, 3)
+        assert transform[1].shape == (3,)
+
+    def coord_mesh_to_phy(self, coord_mesh):
+        if coord_mesh.ndim == 2:
+            return self._transform[0] @ coord_mesh + self._transform[1][:, np.newaxis]
+        elif coord_mesh.ndim == 1:
+            return self._transform[0] @ coord_mesh + self._transform[1]
 
     def read_mesh(self, f_name: str):
-        self._mesh, self._meshReader, self._name2ID = get_mesh_2D(f_name, self._mpi)
+        self._mesh, self._meshReader, self._name2IDAutoAppend = get_mesh_2D(
+            f_name, self._mpi
+        )
+        self._name2ID = self._name2IDAutoAppend.n2id_map
+        self._id2name = {}
+        for name, id in self._name2ID.items():
+            self._id2name[id] = name
         coordsFatherData = np.array(self._mesh.coords.father.data())
         # print(coordsFatherData.shape)
         coordsFatherData = coordsFatherData.reshape((3, -1), order="F")
+        coordsFatherData = self.coord_mesh_to_phy(coordsFatherData)
         coordsMax = coordsFatherData.max(axis=1)
         coordsMin = coordsFatherData.min(axis=1)
         self._MPI.Allreduce(None, coordsMax, op=MPI.MAX)
@@ -29,6 +45,11 @@ class OversetPart2D:
             (self._xyzMin.transpose(), self._xyzMax.transpose())
         ).reshape((2, 3), order="C")
 
+    def obtain_dist_node(self):
+        from OversetCartUtil import obtain_part_local_elem_dists
+
+        self.dist_node = obtain_part_local_elem_dists(self)
+
 
 class OversetBG2D:
     def __init__(self, mpi: DNDS.MPIInfo):
@@ -36,6 +57,7 @@ class OversetBG2D:
         self._MPI = get_mpi4py_comm_from_MPIInfo(mpi)
 
     def set_bg(self, parts: list[OversetPart2D], h: float):
+        self.h = h
         box = np.zeros((2, 3))
         box[0, :] = 1e300
         box[1, :] = -1e300
@@ -101,6 +123,30 @@ class OversetBG2D:
                 f"{self._mpi.rank} nStarts: {self.nStarts4point}, nProcAx: {self.nProcAx}"
             )
 
+    def rank_to_ax_rank(self, rank=None):
+        if rank is None:
+            rank = self._mpi.rank
+        return (rank // self.nProcAx[1], rank % self.nProcAx[1])
+
+    def proc_cell_grid_shape(self):
+        ax_rank = self.rank_to_ax_rank()
+        return tuple(
+            [
+                self.nStarts[i][ax_rank[i] + 1] - self.nStarts[i][ax_rank[i]]
+                for i in range(2)
+            ]
+        )
+
+    def proc_point_grid_shape(self):
+        ax_rank = self.rank_to_ax_rank()
+        return tuple(
+            [
+                self.nStarts4point[i][ax_rank[i] + 1]
+                - self.nStarts4point[i][ax_rank[i]]
+                for i in range(2)
+            ]
+        )
+
     def ijk_to_rank(self, ijk, is_point=False):
         idxs = []
         for iax in range(2):
@@ -119,6 +165,50 @@ class OversetBG2D:
         # print(idxs)
         return self.procMap[tuple(idxs)]
 
+    def obtain_dist_map(self, parts: list[OversetPart2D]):
+
+        MPI = self._MPI
+        mpi = self._mpi
+        from OversetCartUtil import obtain_part_local_dists
+
+        for part in parts:
+            part_local_dists = obtain_part_local_dists(self, part)
+            part_local_dists_items = list(part_local_dists.items())
+
+            ijkv = np.array([v[0] for v in part_local_dists_items]).transpose()
+            if ijkv.size:
+                ranks = list(self.ijk_to_rank(ijkv))
+            else:
+                ranks = []
+            # sendLists = [[]] * self._mpi.size # ! this is wrong!!! the empty lists of each item refs the same
+            sendLists = [[] for _ in range(mpi.size)]
+
+            for i, rank in enumerate(ranks):
+                datac = part_local_dists_items[i]
+                data = ((int(datac[0][0]), int(datac[0][1])), float(datac[1]))
+                sendLists[rank].append(datac)
+                ax_r = self.rank_to_ax_rank(rank)
+
+            recvLists = MPI.alltoall(sendLists)
+            # print(f"{mpi.rank}, {([len(v) for v in sendLists])}, {len(sendLists)}")
+            # return
+
+            ax_ranks = self.rank_to_ax_rank()
+            proc_bg_mesh_dist = np.ones(self.proc_point_grid_shape()) * 1e300
+            for recvL in recvLists:
+                # print(recvL)
+                for g_point, v in recvL:
+                    for ax in range(2):
+                        assert (
+                            g_point[ax] < self.nStarts4point[ax][ax_ranks[ax] + 1]
+                        ), g_point[ax]
+                    l_point = (
+                        g_point[0] - self.nStarts4point[0][ax_ranks[0]],
+                        g_point[1] - self.nStarts4point[1][ax_ranks[1]],
+                    )
+                    proc_bg_mesh_dist[l_point] = v
+            print(f"proc {mpi.rank}: min dist at grid point: {proc_bg_mesh_dist.min()}")
+
 
 if __name__ == "__main__":
     mpi = DNDS.MPIInfo()
@@ -136,15 +226,16 @@ if __name__ == "__main__":
             "NACA0012_H2.cgns",
         )
     )
+    osPart.obtain_dist_node()
 
     osBG = OversetBG2D(mpi)
-    osBG.set_bg([osPart], 1.0 / 10)
+    osBG.set_bg([osPart], 1.0 / 2)
+    assert osBG.procMap[osBG.rank_to_ax_rank()] == mpi.rank
+    osBG.obtain_dist_map([osPart])
 
     if mpi.rank == 0:
-        print(
-            osBG.ijk_to_rank(
-                np.random.randint(0, min(osBG.grid_shape[0:2]) - 1, size=(2, 32))
-            )
+        _ = osBG.ijk_to_rank(
+            np.random.randint(0, min(osBG.grid_shape[0:2]) - 1, size=(2, 32))
         )
 
         print(osBG.ijk_to_rank([v - 1 for v in osBG.grid_shape], is_point=True))
---
+++ test/Geom/OversetCart/OversetCartUtil.py
@@ -0,0 +1,99 @@
+from OversetCart import *
+from CartUtil import *
+import scipy.spatial
+from ElemInterpolate import elem_get_interpolation_base
+
+def get_mesh_bnd_elems(osPart: OversetPart2D):
+    mesh = osPart._mesh
+    bnd_elems = []
+    for iB in range(mesh.NumBnd()):
+        bnd2node = np.array(osPart._mesh.bnd2node[iB], copy=False)
+        assert (bnd2node < mesh.NumNodeProc()).all(), str(bnd2node)
+        nodeList = []  #!2d: list of nodes represent a bnd element
+        for n in bnd2node:
+            nodeList.append(np.array(mesh.coords[n], copy=True))
+        nodeList = np.array(nodeList).transpose()
+        bElemInfo = mesh.bndElemInfo[iB, 0]
+        bnd_elems.append((bElemInfo.getElemType(), bElemInfo.zone, nodeList))
+    return bnd_elems
+
+
+def obtain_part_local_inner_grid_points_dist_dict(
+    self: OversetBG2D, osPart: OversetPart2D
+):
+    local_point_dists = {}
+    mesh = osPart._mesh
+    origin = np.array(self.origins[0:2])
+    h = self.h
+    for iCell in range(mesh.NumCell()):
+        cell2node = np.array(mesh.cell2node[iCell], copy=False)
+        assert (cell2node < mesh.NumNodeProc()).all(), str(cell2node)
+        nodeDists = osPart.dist_node[cell2node]
+        coords = []
+        for iNode in cell2node:
+            coords.append(np.array(mesh.coords[iNode], copy=True))
+        coords = np.array(coords).transpose()
+        coords = osPart.coord_mesh_to_phy(coords)
+        elemInfo = mesh.cellElemInfo[iCell, 0]
+        assert elemInfo.getElemType() in {
+            Geom.Elem.ElemType.Tri3,
+            Geom.Elem.ElemType.Quad4,
+        }  # coords is now polygon
+        gridPoints = single_elem_get_grid_point_2D(origin, h, coords[0:2, :])
+        gridPointsCoords = origin[:, None] + gridPoints * h
+
+        if gridPoints.size:
+            for iP in range(gridPoints.shape[1]):
+
+                p = np.zeros(3)
+                p[:2] = gridPointsCoords[:, iP]
+                distP = elem_get_interpolation_base(elemInfo.getElemType(), coords, p).dot(
+                    nodeDists
+                )
+                local_point_dists[tuple(gridPoints[:, iP])] = distP
+    return local_point_dists
+
+
+def obtain_part_local_elem_dists(self: OversetPart2D):
+    mpi = self._mpi
+    MPI = self._MPI
+    bnd_elems_local = get_mesh_bnd_elems(self)
+    bnd_elems_local_others = MPI.allgather(bnd_elems_local)
+
+    bnd_geoms = []
+    for bnd_part in bnd_elems_local_others:
+        for e_tuple in bnd_part:
+            assert e_tuple[0] == Geom.Elem.ElemType.Line2
+            bnd_geoms.extend([e_tuple[2][:, 0], e_tuple[2][:, 1]])
+    tree = scipy.spatial.KDTree(np.array(bnd_geoms))
+
+    mesh = self._mesh
+    dists = np.ones((mesh.NumNodeProc()), dtype=np.float64) * 1e-300
+    coordsFatherData = np.array(self._mesh.coords.father.data())
+    coordsFatherData = coordsFatherData.reshape((3, -1), order="F")
+    coordsFatherData = self.coord_mesh_to_phy(coordsFatherData)
+
+    distq, idx = tree.query(coordsFatherData.T)
+    dists[0 : mesh.NumNode()] = distq
+
+    coordsSonData = np.array(self._mesh.coords.son.data())
+    coordsSonData = coordsSonData.reshape((3, -1), order="F")
+    coordsSonData = self.coord_mesh_to_phy(coordsSonData)
+
+    distq, idx = tree.query(coordsSonData.T)
+    dists[mesh.NumNode() :] = distq
+    print(f"{mpi.rank}: maxDist: {dists.max()}")
+    return dists
+
+
+def obtain_part_local_dists(self: OversetBG2D, osPart: OversetPart2D):
+    mpi = self._mpi
+    MPI = self._MPI
+    origin = np.array(self.origins[0:2])
+    h = self.h
+    mesh = osPart._mesh
+    local_point_dists = obtain_part_local_inner_grid_points_dist_dict(self, osPart)
+
+    print(f"points covered: {mpi.rank}, {len(local_point_dists)}")
+    
+    return local_point_dists
