4a3ea87ea9b2156b0bcb58960f241847d15272fe
HarryZhou <2373256746@qq.com>
2025-12-02 19:29:54 +0800

    euler: logging is now partitioned

--- src/DNDS/CsvLog.hpp
+++ src/DNDS/CsvLog.hpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 
 #include "Defines.hpp"
 
@@ -45,13 +46,37 @@ namespace DNDS
         std::vector<std::string> titles;
         std::unique_ptr<std::ostream> pOs;
         int64_t n_line{0};
+        std::string log_name_bare;
+        std::string log_name_suffix;
 
         std::string delim = ",";
+        int iBlock = 0;
+        int64_t n_line_max = INT64_MAX;
+        void update_ofstream(const std::string &fname)
+        {
+            std::filesystem::path outFile{fname};
+            std::filesystem::create_directories(outFile.parent_path() / ".");
+            pOs = std::make_unique<std::ofstream>(fname);
+            DNDS_check_throw_info(*pOs, "csv file [" + fname + "] did not open");
+        }
 
     public:
         template <class T_titles>
         CsvLog(T_titles &&n_titles, std::unique_ptr<std::ostream> n_pOs)
-            : titles(std::forward<T_titles>(n_titles)), pOs(std::move(n_pOs)){};
+            : titles(std::forward<T_titles>(n_titles)), pOs(std::move(n_pOs)) {}
+
+        template <class T_titles>
+        CsvLog(T_titles &&n_titles,
+               const std::string &n_log_name_bare,
+               const std::string &n_log_name_suffix = ".csv",
+               int64_t n_n_line_max = 10000)
+            : titles(std::forward<T_titles>(n_titles)),
+              log_name_bare(n_log_name_bare),
+              log_name_suffix(n_log_name_suffix),
+              n_line_max(n_n_line_max)
+        {
+            update_ofstream(log_name_bare + n_log_name_suffix);
+        }
 
         template <class TMap>
         void WriteLine(TMap &&title_to_value, int nPrecision)
@@ -63,6 +88,13 @@ namespace DNDS
                 (*pOs) << title_to_value[titles[i]] << ((i == (titles.size() - 1)) ? "" : ",");
             (*pOs) << std::endl;
             n_line++;
+            if (n_line >= n_line_max)
+            {
+                n_line = 0;
+                iBlock++;
+                DNDS_check_throw_info(log_name_bare.length(), "need to give a log name");
+                update_ofstream(log_name_bare + "_" + std::to_string(iBlock) + log_name_suffix);
+            }
         }
 
         void WriteTitle()
